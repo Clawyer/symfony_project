07d32bc81e3979cfee68bffa33299068ecdb6d3c
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-04-08 12:42:01 +0200

    init project

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,18 @@
+<?php
+// src/Controller/ProfilController.php;
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProfilController extends AbstractController
+{
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function profil(): Response
+    {
+        return $this->render('profil/profil.html.twig');
+    }
+}
